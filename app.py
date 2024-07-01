b2ad3a5cad3f0dd7d91c94e9c90243174c10ee5d
rubenreut06 <ruben.reut.2024@mumail.ie>
2024-07-01 16:32:31 +0100

    Fix import issue for send_from_directory

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import os
 import random
@@ -14,7 +14,6 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import yt_dlp
-import zipfile
 
 app = Flask(__name__, static_folder='static')
 CORS(app)
@@ -29,8 +28,9 @@ def index():
 def download_reels():
     data = request.json
     urls = data['urls']
+    output_directory = 'reels'
+    os.makedirs(output_directory, exist_ok=True)
 
-    output_directory = tempfile.mkdtemp()
     NUM_CORES = multiprocessing.cpu_count()
     MAX_WORKERS = max(1, NUM_CORES - 2)
 
@@ -42,14 +42,7 @@ def download_reels():
         for future in as_completed(futures):
             future.add_done_callback(update_progress_bar)
 
-    zip_filename = os.path.join(output_directory, 'reels.zip')
-    with zipfile.ZipFile(zip_filename, 'w') as zipf:
-        for root, dirs, files in os.walk(output_directory):
-            for file in files:
-                if file != 'reels.zip':
-                    zipf.write(os.path.join(root, file), file)
-
-    return send_file(zip_filename, as_attachment=True, attachment_filename='reels.zip')
+    return jsonify({"status": "success"})
 
 def download_reel(url, idx, output_directory):
     ydl_opts = {
